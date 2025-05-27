1ec93ab29523ae8d481ae36fccc16e48458c5af9
DusanSpasojevic98 <dusan.spasojevic.1998@gmail.com>
2025-05-27 15:04:14 +0200

    Uncommented confetti code

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { ThemedText } from "@/src/components/ThemedText";
 import React from "react";
 import { View } from "react-native";
-// import { Confetti } from "typegpu-confetti/react-native";
+import { Confetti } from "typegpu-confetti/react-native";
 
 //App works when confetti code is commented out, but crashes when confetti code is uncommented.
 const index = () => {
@@ -16,7 +16,7 @@ const index = () => {
         }}>
         Confetti!!!
       </ThemedText>
-      {/* <Confetti initParticleAmount={200} /> */}
+      <Confetti initParticleAmount={200} />
     </View>
   );
 };
